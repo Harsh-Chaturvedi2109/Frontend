a76d85347f7dad286f974ee692baff75f6d6aa5f
Harsh Chaturvedi <harshchaturvedi2109@gmail.com>
2024-04-26 16:57:19 +0530

    fixed some security bugs

--- src/components/loginComponent/loginForm.js
+++ src/components/loginComponent/loginForm.js
@@ -1,81 +1,93 @@
 import { Link } from "react-router-dom";
-import  "./loginForm.css";
+import "./loginForm.css";
 import { useNavigate } from "react-router-dom";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Spin } from "antd";
 
 function Login() {
-
-  const initialState ={
-    email :"",
-    password :""
-}
-const [loginForm, setLoginForm] = useState(initialState);
-const[error,setError] = useState("");
-const navigate = useNavigate();
-const [loading,setLoading] = useState(false);
+  const initialState = {
+    email: "",
+    password: "",
+  };
+  const [loginForm, setLoginForm] = useState(initialState);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     document.body.classList.add("loginForm-body");
     return () => {
-        document.body.classList.remove("loginForm-body");
+      document.body.classList.remove("loginForm-body");
     };
-}, []);
-    async function handleSubmit(e){
-        e.preventDefault();
-        setLoading(true);
-        console.log(loginForm);
-        const response = await fetch("https://backend-6tqr.onrender.com/login",{
-            'method':'POST',
-            body:JSON.stringify(loginForm),
-            headers:{
-                'Content-Type':'application/json'
-            }
-        })
+  }, []);
 
-        const data = await response.json();
-        console.log(data);
-        if(response.ok){
-            localStorage.setItem("Authorization",data.token);
-            localStorage.setItem("Role",data.role);
-            setLoading(false);
-            navigate('/Home');
-        }
-        else{
-            setError(data.message);
-            console.log("Error",data.err);
-        }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    console.log(loginForm);
+    const response = await fetch("https://backend-6tqr.onrender.com/login", {
+      method: "POST",
+      body: JSON.stringify(loginForm),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      const token = response.headers.get("Authorization");
+      const data = await response.json();
+      localStorage.setItem("Authorization", token);
+      localStorage.setItem("Role", data.role);
+      setLoading(false);
+      navigate("/Home");
+    } else {
+      const data = await response.json();
+      setError(data.message);
+      console.log("Error", data.err);
     }
-    function handleChange(e){
-        console.log(e.target.name,e.target.value);
-        setLoginForm({
-            ...loginForm,
-            [e.target.name]:e.target.value
-        })
-        setError("");
-    }
+  }
+
+  function handleChange(e) {
+    console.log(e.target.name, e.target.value);
+    setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
+    setError("");
+  }
+
   return (
-    <div className="loginContainer" style={{backgroundColor:"black"}}>
+    <div className="loginContainer" style={{ backgroundColor: "black" }}>
       <div className="background">
         <div className="shape" />
         <div className="shape" />
       </div>
-      {loading && (<div>
-      <Spin size="large" fullscreen = {true}/>
-      </div>)}
+      {loading && <div>
+        <Spin size="large" fullscreen={true} />
+      </div>}
       <form onSubmit={handleSubmit}>
         <h3>Login Here</h3>
         <label htmlFor="email">Email</label>
-        <input type="text" placeholder="Email" id="email" name="email" value={loginForm.email} onChange={handleChange} />
+        <input
+          type="text"
+          placeholder="Email"
+          id="email"
+          name="email"
+          value={loginForm.email}
+          onChange={handleChange}
+        />
         <label htmlFor="password">Password</label>
-        <input type="password" placeholder="Password" id="password" name="password" value={loginForm.password} onChange={handleChange} />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          name="password"
+          value={loginForm.password}
+          onChange={handleChange}
+        />
         <button>Log In</button>
         <div>
-            Don't have an Account?<Link to="/">Register</Link>
+          Don't have an Account?<Link to="/">Register</Link>
         </div>
         {error && <p style={{ color: "white" }}>{error}</p>}
       </form>
-      
     </div>
   );
 }
